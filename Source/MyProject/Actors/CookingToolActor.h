e128ec184432a7e582fae472f32de6dd04135375
謙 <chesteras452@gmail.com>
2026-05-18 13:26:20 +0800

    食材在鍋裡&顧客目標

--- Source/MyProject/Actors/CookingToolActor.h
+++ Source/MyProject/Actors/CookingToolActor.h
@@ -3,8 +3,10 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "../InteractInterface.h"
+#include "../GameDataStructs.h"
+#include "Camera/CameraComponent.h"
 
-#include "CookingToolActor.generated.h" // 👈 這行永遠要是最後一個 include
+#include "CookingToolActor.generated.h"
 
 class UQTEComponent;
 
@@ -17,9 +19,11 @@ public:
 	virtual void Interact(AActor* Interactor) override;
 	virtual FText GetInteractPrompt() const override;
 
+	// 接收食材的函數 (內部已新增 IsSliceable 驗證邏輯)
 	UFUNCTION(BlueprintCallable, Category = "Cooking")
 	void AcceptIngredients(AActor* Interactor, TArray<FName> SelectedItems);
 
+	// 打開料理 UI
 	UFUNCTION(BlueprintImplementableEvent, Category = "Cooking")
 	void OnOpenCookingUI(AActor* Interactor);
 
@@ -31,7 +35,7 @@ public:
 	UFUNCTION(BlueprintImplementableEvent, Category = "QTE")
 	void OnQTEStarted();
 
-	// 👇 讓藍圖可以呼叫隨機觸發 QTE 的函式
+	// 讓藍圖可以呼叫隨機觸發 QTE 的函式
 	UFUNCTION(BlueprintCallable, Category = "QTE")
 	void TriggerRandomQTE();
 
@@ -42,20 +46,34 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Cooking")
 	TArray<FName> StoredIngredients;
 
-	// 👇 隨機 QTE 所需的變數與函式 (確保只有這裡有一份)
+	// 視覺化食材相關變數
+	UPROPERTY(EditAnywhere, Category = "Data")
+	UDataTable* IngredientDataTable;
+
+	// 用來記錄我們動態生成的食材模型
+	UPROPERTY()
+	TArray<UStaticMeshComponent*> DisplayedIngredientMeshes;
+
+	// 廚具專屬的俯視相機組件
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Cooking|Camera")
+	UCameraComponent* TopDownCamera;
+
+	// 隨機 QTE 所需的變數
 	FTimerHandle QTE_TimerHandle;
 
 	UPROPERTY()
 	int32 QTECount = 0;
+
 	UPROPERTY(BlueprintReadWrite, Category = "QTE")
 	int32 SuccessfulQTECount = 0;
 
-	// 👇 烹飪結束時，通知藍圖彈出結算畫面的事件
+	// 烹飪結束時，通知藍圖彈出結算畫面的事件
 	UFUNCTION(BlueprintImplementableEvent, Category = "Cooking")
 	void OnCookingFinished(int32 FinalSuccessCount);
+
 	UPROPERTY()
 	AActor* CurrentInteractor;
 
-	// 烹飪完成的函式
+	// 烹飪完成的內部邏輯處理 (內部已新增產出 _Cooked 食材邏輯)
 	void FinishCooking();
 };
